ee253cccfcaf7571ed36b8b9512a137a8bbd20a9
André Vidal <andre.vidal@rockcontent.com>
2021-02-18 21:30:36 -0300

    Servindo arquivos estáticos com o módulo serve-static

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import connect from "connect";
 import morgan from "morgan";
+import serveStatic from "serve-static";
 
 const app = connect();
 app.listen(3000);
 
 app.use(morgan("dev"));
+app.use(serveStatic("./public"));
+app.use(incluiCabecalhos);
 app.use(helloWorld);
 
 console.log("Servidor escutando em http://localhost:3000");
 
-function helloWorld(request, response, next) {
+function incluiCabecalhos(request, response, next) {
   response.setHeader("Content-Type", "text/html");
+  response.write("<link rel=stylesheet href=estilo.css />");
+  next();
+}
+
+function helloWorld(request, response, next) {
   response.end("<h1>Hello, World!</h1>");
 }
