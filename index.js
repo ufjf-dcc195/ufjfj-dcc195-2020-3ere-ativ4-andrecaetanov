af62de329e0f41445e444e345b75687dfe5c618c
André Vidal <andre.vidal@rockcontent.com>
2021-02-18 21:19:52 -0300

    Registro de log para desenvolvimento com o módulo morgan

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import connect from "connect";
+import morgan from "morgan";
 
 const app = connect();
 app.listen(3000);
 
+app.use(morgan("dev"));
+app.use(helloWorld);
+
 console.log("Servidor escutando em http://localhost:3000");
+
+function helloWorld(request, response, next) {
+  response.setHeader("Content-Type", "text/html");
+  response.end("<h1>Hello, World!</h1>");
+}
